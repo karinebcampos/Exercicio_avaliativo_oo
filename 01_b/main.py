f4fcd2ed45b303a796462fd19a78d88a8407187b
Karine Campos <kaarine_campos@hotmail.com>
2021-08-27 20:12:24 -0300

    Atividade 1_b

---
+++ 01_b/main.py
@@ -0,0 +1,5 @@
+from soma_numero import Soma
+
+numeros = [1, 2 ,3, 4]
+resultado = Soma.soma_numeros(numeros)
+print('Soma de todos os valores do vetor = ' + str(resultado))
---
+++ 01_b/soma_numero.py
@@ -0,0 +1,10 @@
+class Soma:
+
+    @staticmethod
+    def soma_numeros(numeros):
+        resultado = 0
+        for numero in numeros:
+            resultado = resultado + numero
+        return resultado
+
+
